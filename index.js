e15bd78d83d88db049b81a0ad5f491f3bf69340e
PC3961 <cpranavss65@gmail.com>
2023-11-18 12:47:53 +0530

    Add Sounds to button Clicks

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-var buttonColours =["red", "blue", "green", "yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-$(".btn").click(function(){
+$(".btn").click(function () {
 
-    varUserChosenColour = $(this).attr("id");
+    var UserChosenColour = $(this).attr("id");
     userClickedPattern.push(varUserChosenColour);
-    
+    playsound(UserChosenColour);
 })
 function nextSequence() {
-    var randomNumber= Math.floor(Math.random(0)*4);
+    var randomNumber = Math.floor(Math.random(0) * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+    
+}
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 
 }
