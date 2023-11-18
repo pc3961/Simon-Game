3f8c54d7b94f8855c9078988d58ffa7378f97c16
PC3961 <cpranavss65@gmail.com>
2023-11-18 12:44:46 +0530

    Checked Whether the button is pressed

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 var buttonColours =["red", "blue", "green", "yellow"];
-
 var gamePattern = [];
+var userClickedPattern = [];
 
+$(".btn").click(function(){
 
-function nextSequence(randomNumber) {
-    randomNumber= Math.floor(Math.random(0)*4);
-
+    varUserChosenColour = $(this).attr("id");
+    userClickedPattern.push(varUserChosenColour);
+    
+})
+function nextSequence() {
+    var randomNumber= Math.floor(Math.random(0)*4);
     var randomChosenColour = buttonColours[randomNumber];
-
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
     var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     audio.play();
-
-
 
 }
